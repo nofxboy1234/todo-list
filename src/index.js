b3358db2fac8f3f67ae1fedaf598a8b8bd01bc16
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-08 11:26:14 +0200

    Create planning diagrams and import module into index.js module

--- src/index.js
+++ src/index.js
@@ -14,41 +14,10 @@ import json from './data.json5';
 
 import printMe from './print.js';
 
-console.log(toml.title);
-console.log(toml.owner.name);
+import { createTodoController } from './todosController.js';
 
-console.log(yaml.title);
-console.log(yaml.owner.name);
-
-console.log(json.title);
-console.log(json.owner.name);
-
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-  element.appendChild(btn);
-
-  console.log(myName('Dylan'));
-  console.log(functionOne());
-  console.log(functionTwo());
-
-  console.log(Data);
-  console.log(Notes);
-  console.log(Data2);
-
-  return element;
-}
-
-document.body.appendChild(component());
+const todoController = createTodoController();
+todoController.create({
+  title: 'Feed the dogs',
+  description: 'Make sure the dogs are happy!',
+});
---
+++ src/todosController.js
@@ -0,0 +1,17 @@
+// new
+const createTodoController = () => {
+  // create
+  // edit
+  // update
+  // destroy
+  // index
+  // show
+  const create = (todoParams) => {
+    // const todo = Todo.create(todoParams);
+    console.log(todoParams);
+  };
+
+  return { create };
+};
+
+export { createTodoController };
