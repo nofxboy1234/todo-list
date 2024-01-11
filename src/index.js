3486f958d6d596e9ca0dc44148b9a4e3e32527bb
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-11 16:19:42 +0200

    Start application layout using flexbox

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import { TodosController } from './controllers/todosController.js';
 import { Todo } from './models/todo.js';
+import { createLayout } from './views/layouts/application.js';
 import './style.css';
 
 const todosController = TodosController;
 
+createLayout();
+
 // todosController.new();
 
-const checkList = { 'Fill water bowl': false, 'Fill food bowl': false };
-todosController.create(
-  'Feed the dogs',
-  'Make sure the dogs are happy!',
-  '2024-01-31',
-  'High',
-  checkList,
-  'Home'
-);
+// const checkList = { 'Fill water bowl': false, 'Fill food bowl': false };
+// todosController.create(
+//   'Feed the dogs',
+//   'Make sure the dogs are happy!',
+//   '2024-01-31',
+//   'High',
+//   checkList,
+//   'Home'
+// );
 
-todosController.edit(Todo.last().id);
+// todosController.edit(Todo.last().id);
 // const checkList2 = { 'Fill water bowl': false, 'Fill food bowl': false };
 // todosController.create(
 //   'Feed the cat',
---
+++ src/views/layouts/application.js
@@ -0,0 +1,15 @@
+const createLayout = () => {
+  const flexContainer = document.createElement('div');
+  flexContainer.classList.add('flex-container');
+  document.body.appendChild(flexContainer);
+
+  const flexItem1 = document.createElement('div');
+  flexItem1.classList.add('flex-item');
+  flexContainer.appendChild(flexItem1);
+
+  const flexItem2 = document.createElement('div');
+  flexItem2.classList.add('flex-item');
+  flexContainer.appendChild(flexItem2);
+};
+
+export { createLayout };
