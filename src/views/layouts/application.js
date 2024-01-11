c61ab7794fb7db7ee0dbc9db2b579c7a149d2aff
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-11 17:21:19 +0200

    Adjust flex settings

--- src/views/layouts/application.js
+++ src/views/layouts/application.js
@@ -4,11 +4,11 @@ const createLayout = () => {
   document.body.appendChild(flexContainer);
 
   const flexItem1 = document.createElement('div');
-  flexItem1.classList.add('flex-item');
+  flexItem1.classList.add('flex-item', 'flex-item-left');
   flexContainer.appendChild(flexItem1);
 
   const flexItem2 = document.createElement('div');
-  flexItem2.classList.add('flex-item');
+  flexItem2.classList.add('flex-item', 'flex-item-right');
   flexContainer.appendChild(flexItem2);
 };
 
