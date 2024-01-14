c0043298f106d8ce6568db6e4492cb4338d9945e
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-14 18:36:21 +0200

    Start creating form for new todos

--- src/views/todos/_form.js
+++ src/views/todos/_form.js
@@ -8,39 +8,70 @@ const formPartial = (todo) => {
     todo.project = 'blank';
   }
 
-  const todoDiv = document.createElement('p');
-  todoDiv.classList.add('new-todo-form');
+  const todoForm = document.createElement('form');
+  todoForm.classList.add('new-todo-form');
+  // todoForm.action = 'todos/new';
+  // todoForm.method = 'post';
 
+  const titleDiv = document.createElement('div');
+  const titleLabel = document.createElement('label');
+  titleLabel.textContent = 'title:';
+  titleLabel.htmlFor = 'titleID';
+  titleDiv.appendChild(titleLabel);
   const titleInput = document.createElement('input');
-  titleInput.textContent = todo.title;
-  todoDiv.appendChild(titleInput);
+  titleInput.type = 'text';
+  titleInput.id = 'titleID';
+  titleInput.name = 'title';
+  titleInput.value = todo.title;
+  titleDiv.appendChild(titleInput);
+  todoForm.appendChild(titleDiv);
 
   const descriptionDiv = document.createElement('div');
-  descriptionDiv.textContent = todo.description;
-  todoDiv.appendChild(descriptionDiv);
+  const descriptionLabel = document.createElement('label');
+  descriptionLabel.textContent = 'description:';
+  descriptionLabel.htmlFor = 'descriptionID';
+  descriptionDiv.appendChild(descriptionLabel);
+  const descriptionInput = document.createElement('textarea');
+  descriptionInput.id = 'descriptionID';
+  descriptionInput.name = 'description';
+  descriptionInput.value = todo.description;
+  descriptionDiv.appendChild(descriptionInput);
+  todoForm.appendChild(descriptionDiv);
 
-  const dueDateDiv = document.createElement('div');
-  dueDateDiv.textContent = todo.dueDate;
-  todoDiv.appendChild(dueDateDiv);
+  const submitDiv = document.createElement('div');
+  const submitButton = document.createElement('button');
+  submitButton.type = 'submit';
+  submitButton.textContent = 'Submit';
+  submitDiv.appendChild(submitButton);
+  todoForm.appendChild(submitDiv);
 
-  const priorityDiv = document.createElement('div');
-  priorityDiv.textContent = todo.priority;
-  todoDiv.appendChild(priorityDiv);
+  // const descriptionDiv = document.createElement('div');
+  // const descriptionTextArea = document.createElement('textarea');
+  // descriptionTextArea.value = todo.description;
+  // todoForm.appendChild(descriptionTextArea);
 
-  const checkListDiv = document.createElement('div');
-  for (const [key, value] of Object.entries(todo.checkList)) {
-    const checkListEntryDiv = document.createElement('div');
-    checkListEntryDiv.textContent = `- ${key}: ${value}`;
-    checkListDiv.appendChild(checkListEntryDiv);
-  }
+  // const dueDateDiv = document.createElement('div');
+  // dueDateDiv.textContent = todo.dueDate;
+  // todoForm.appendChild(dueDateDiv);
 
-  todoDiv.appendChild(checkListDiv);
+  // const priorityDiv = document.createElement('div');
+  // priorityDiv.textContent = todo.priority;
+  // todoForm.appendChild(priorityDiv);
 
-  const projectDiv = document.createElement('div');
-  projectDiv.textContent = todo.project;
-  todoDiv.appendChild(projectDiv);
+  // const checkListDiv = document.createElement('div');
+  // for (const [key, value] of Object.entries(todo.checkList)) {
+  //   const checkListEntryDiv = document.createElement('div');
+  //   checkListEntryDiv.textContent = `- ${key}: ${value}`;
+  //   checkListDiv.appendChild(checkListEntryDiv);
+  // }
 
-  return todoDiv;
+  // todoForm.appendChild(checkListDiv);
+
+  // const projectDiv = document.createElement('div');
+  // projectDiv.textContent = todo.project;
+  // todoForm.appendChild(projectDiv);
+
+  return todoForm;
 };
 
 export { formPartial };
