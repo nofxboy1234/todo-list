7ed23962f6ee4e4c0681af46366dbfd26ebd1461
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-14 19:10:13 +0200

    Add priority select to form

--- src/views/todos/_form.js
+++ src/views/todos/_form.js
@@ -50,16 +50,35 @@ const formPartial = (todo) => {
   dueDateDiv.appendChild(dueDateInput);
   todoForm.appendChild(dueDateDiv);
 
+  const priorityDiv = document.createElement('div');
+  const priorityLabel = document.createElement('label');
+  priorityLabel.textContent = 'priority:';
+  priorityLabel.htmlFor = 'priorityID';
+  priorityDiv.appendChild(priorityLabel);
+  const prioritySelect = document.createElement('select');
+  prioritySelect.id = 'priorityID';
+  prioritySelect.name = 'priority';
+  const lowOption = document.createElement('option');
+  lowOption.value = 'low';
+  lowOption.text = 'low';
+  prioritySelect.add(lowOption);
+  const mediumOption = document.createElement('option');
+  mediumOption.value = 'medium';
+  mediumOption.text = 'medium';
+  prioritySelect.add(mediumOption);
+  const highOption = document.createElement('option');
+  highOption.value = 'high';
+  highOption.text = 'high';
+  prioritySelect.add(highOption);
+  priorityDiv.appendChild(prioritySelect);
+  todoForm.appendChild(priorityDiv);
+
   const submitDiv = document.createElement('div');
   const submitButton = document.createElement('button');
   submitButton.type = 'submit';
   submitButton.textContent = 'Submit';
   submitDiv.appendChild(submitButton);
   todoForm.appendChild(submitDiv);
-
-  // const priorityDiv = document.createElement('div');
-  // priorityDiv.textContent = todo.priority;
-  // todoForm.appendChild(priorityDiv);
 
   // const checkListDiv = document.createElement('div');
   // for (const [key, value] of Object.entries(todo.checkList)) {
