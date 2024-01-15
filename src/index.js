b48581efd78966d1f37c0beb7d9ea7d96357b75a
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-15 14:36:12 +0200

    Remove commented code

--- src/index.js
+++ src/index.js
@@ -2,28 +2,3 @@ import { createLayout } from './views/layouts/application.js';
 import './style.css';
 
 createLayout();
-
-// todosController.new();
-
-// const checkList = { 'Fill water bowl': false, 'Fill food bowl': false };
-// todosController.create(
-//   'Feed the dogs',
-//   'Make sure the dogs are happy!',
-//   '2024-01-31',
-//   'High',
-//   checkList,
-//   'Home'
-// );
-
-// todosController.edit(Todo.last().id);
-// const checkList2 = { 'Fill water bowl': false, 'Fill food bowl': false };
-// todosController.create(
-//   'Feed the cat',
-//   'Make sure the cats are happy!',
-//   '2024-01-30',
-//   'Medium',
-//   checkList2,
-//   'Home'
-// );
-
-// todosController.show(Todo.last().id);
