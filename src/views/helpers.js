52d3d98b542d6d13b26daf18666c93621f9ecbf9
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-16 15:00:59 +0200

    Move functions to helpers module

--- src/views/helpers.js
+++ src/views/helpers.js
@@ -1,4 +1,4 @@
-import { contentContainer } from "./layouts/application";
+import { contentContainer } from './layouts/application';
 
 const clearContent = () => {
   while (contentContainer.firstChild) {
@@ -7,4 +7,70 @@ const clearContent = () => {
   }
 };
 
-export { clearContent };
+const createLabel = (text, forID) => {
+  const label = document.createElement('label');
+  label.textContent = text;
+  label.htmlFor = forID;
+  return label;
+};
+
+const createInput = (type, id, name, value) => {
+  const input = document.createElement('input');
+  input.type = type;
+  input.id = id;
+  input.name = name;
+  input.value = value;
+  return input;
+};
+
+const createTextArea = (id, name, value) => {
+  const textArea = document.createElement('textarea');
+  textArea.id = id;
+  textArea.name = name;
+  textArea.value = value;
+  return textArea;
+};
+
+const createOption = (value, text) => {
+  const option = document.createElement('option');
+  option.value = value;
+  option.text = text;
+  return option;
+};
+
+const createSelect = (id, name, options) => {
+  const select = document.createElement('select');
+  select.id = id;
+  select.name = name;
+  options.forEach((option) => {
+    select.add(createOption(option, option));
+  });
+  return select;
+};
+
+const createDataList = (id, options) => {
+  const dataList = document.createElement('datalist');
+  dataList.id = id;
+  options.forEach((option) => {
+    dataList.appendChild(createOption(option, option));
+  });
+  return dataList;
+};
+
+const createButton = (type, text) => {
+  const button = document.createElement('button');
+  button.type = type;
+  button.textContent = text;
+  return button;
+};
+
+export {
+  clearContent,
+  createLabel,
+  createInput,
+  createTextArea,
+  createOption,
+  createSelect,
+  createDataList,
+  createButton
+};
--- src/views/todos/_form.js
+++ src/views/todos/_form.js
@@ -1,64 +1,15 @@
 import { router } from '../../router';
 import { Todo } from '../../models/todo';
+import {
+  createLabel,
+  createInput,
+  createTextArea,
+  createSelect,
+  createDataList,
+  createButton,
+} from '../helpers';
 
 const redirectTo = (path, ...params) => router.redirectTo(path, ...params);
-
-const createLabel = (text, forID) => {
-  const label = document.createElement('label');
-  label.textContent = text;
-  label.htmlFor = forID;
-  return label;
-};
-
-const createInput = (type, id, name, value) => {
-  const input = document.createElement('input');
-  input.type = type;
-  input.id = id;
-  input.name = name;
-  input.value = value;
-  return input;
-};
-
-const createTextArea = (id, name, value) => {
-  const textArea = document.createElement('textarea');
-  textArea.id = id;
-  textArea.name = name;
-  textArea.value = value;
-  return textArea;
-};
-
-const createOption = (value, text) => {
-  const option = document.createElement('option');
-  option.value = value;
-  option.text = text;
-  return option;
-};
-
-const createSelect = (id, name, options) => {
-  const select = document.createElement('select');
-  select.id = id;
-  select.name = name;
-  options.forEach((option) => {
-    select.add(createOption(option, option));
-  });
-  return select;
-};
-
-const createDataList = (id, options) => {
-  const dataList = document.createElement('datalist');
-  dataList.id = id;
-  options.forEach((option) => {
-    dataList.appendChild(createOption(option, option));
-  });
-  return dataList;
-};
-
-const createButton = (type, text) => {
-  const button = document.createElement('button');
-  button.type = type;
-  button.textContent = text;
-  return button;
-};
 
 const formPartial = (todo) => {
   const todoCommonData = () => {
