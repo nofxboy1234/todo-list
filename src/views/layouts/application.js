c7a69b49ce7621c0da11f1e4d8fc2b7f33d9869c
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-16 10:33:24 +0200

    Refactor application layout

--- src/views/layouts/application.js
+++ src/views/layouts/application.js
@@ -6,17 +6,22 @@ const newTodo = () => {
   redirectTo('/todos/new');
 };
 
-const flexContainer = document.createElement('div');
-flexContainer.classList.add('flex-container');
-document.body.appendChild(flexContainer);
+const createFlexContainer = (...classList) => {
+  const flexContainer = document.createElement('div');
+  flexContainer.classList.add(...classList);
+  return flexContainer;
+};
 
-const menuContainer = document.createElement('div');
-menuContainer.classList.add('flex-item', 'flex-item-left');
-flexContainer.appendChild(menuContainer);
+const nestContainer = (parent, child) => parent.appendChild(child);
 
-const contentContainer = document.createElement('div');
-contentContainer.classList.add('flex-item', 'flex-item-right');
-flexContainer.appendChild(contentContainer);
+const flexContainer = createFlexContainer('flex-container');
+nestContainer(document.body, flexContainer);
+
+const menuContainer = createFlexContainer('flex-item', 'flex-item-left');
+nestContainer(flexContainer, menuContainer);
+
+const contentContainer = createFlexContainer('flex-item', 'flex-item-right');
+nestContainer(flexContainer, contentContainer);
 
 const createLayout = () => {
   const headingItem = document.createElement('div');
