3c56593ec78fa3d39c16f66c420037c872a9f919
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-18 09:31:17 +0200

    Remove unused import

--- src/views/todos/show.js
+++ src/views/todos/show.js
@@ -1,7 +1,6 @@
 import { contentContainer } from '../layouts/application';
 import { clearContent } from '../helpers';
 import { todoPartial } from './_todo';
-import { router } from '../../router';
 
 const render = (todo) => {
   if (!todo) return;
