ededc5fc2402a77f0c49a4959802ae9afff7bac8
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-18 09:35:46 +0200

    Refactor delete button creation in todos index view

--- src/views/todos/index.js
+++ src/views/todos/index.js
@@ -1,36 +1,34 @@
 import { contentContainer } from '../layouts/application';
-import { clearContent } from '../helpers';
+import { clearContent, createButton } from '../helpers';
 import { redirectTo } from '../../helpers';
 
 const render = (todos) => {
   clearContent();
   todos.forEach((todo) => {
-    const todoDiv = document.createElement('p');
+    const todoParagraph = document.createElement('p');
 
     const showTodo = () => {
       redirectTo('/todos', todo.id);
     };
 
     const destroyTodo = () => {
-      // console.log('destroy');
       redirectTo('/todos', todo.id, 'DELETE');
     };
 
     const titleDiv = document.createElement('div');
     titleDiv.textContent = todo.title;
     titleDiv.addEventListener('click', showTodo);
-    todoDiv.appendChild(titleDiv);
+    todoParagraph.appendChild(titleDiv);
 
     const dueDateDiv = document.createElement('div');
     dueDateDiv.textContent = todo.dueDate;
-    todoDiv.appendChild(dueDateDiv);
+    todoParagraph.appendChild(dueDateDiv);
 
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'DELETE';
+    const deleteButton = createButton('button', 'DELETE');
     deleteButton.addEventListener('click', destroyTodo);
-    todoDiv.appendChild(deleteButton);
+    todoParagraph.appendChild(deleteButton);
 
-    contentContainer.appendChild(todoDiv);
+    contentContainer.appendChild(todoParagraph);
   });
 };
 
