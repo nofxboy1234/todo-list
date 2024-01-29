057cc7702f1aee81b3eb65dce4babea7e0a7d704
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-29 11:43:57 +0200

    Create separate OtherController using composition

--- src/controllers/projectsController.js
+++ src/controllers/projectsController.js
@@ -1,8 +1,6 @@
 import { Project } from '../models/project';
 import { params } from '../parameters/projectParameters';
-import {
-  createController,
-} from './controller';
+import { createController } from './controller';
 
 const permittedParams = ['name'];
 
@@ -13,5 +11,26 @@ const ProjectsController = createController(
   params,
   permittedParams
 );
+
+const Controller = createController(
+  'project',
+  'projects',
+  Project,
+  params,
+  permittedParams
+);
+
+const OtherController = {
+  // new: Controller.new,
+  ...Controller.new,
+  create: function () {
+    //
+  },
+  ...Controller.index,
+  ...Controller.show,
+  ...Controller.edit,
+  ...Controller.update,
+  ...Controller.destroy,
+};
 
 export { ProjectsController };
