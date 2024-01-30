6ed01586e52327a1aefafdced39291f2e3d947fe
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-30 14:41:42 +0200

    Create Default project at startup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import { createLayout } from './views/layouts/application';
 import './style.css';
+import { Project } from './models/project';
 
+function createDefaultProject() {
+  const project = Project.new(defaultProjectData());
+  project.save();
+}
+
+function defaultProjectData() {
+  return {
+    id: 1,
+    project: {
+      name: 'Default',
+    },
+  };
+}
+
+createDefaultProject();
 createLayout();
