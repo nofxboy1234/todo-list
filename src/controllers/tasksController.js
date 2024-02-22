6e65ad42c3599b0672cf1a9f28bb101af773fbdd
Dylan Palmboom <nofxboy1234@gmail.com>
2024-02-22 09:54:34 +0200

    Add task to data.editedTasks and data.destroyedTasks when appropriate

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -11,19 +11,42 @@ const createTaskInTodoParams = (task) => {
   const tempTodoParams = todoParams;
   tempTodoParams.data.tasks.push(task);
 };
+
+const addTaskToEditedTasks = (todoParamsTask) => {
+  if (!todoParams.data.editedTasks) {
+    todoParams.data.editedTasks = [];
+  }
+  todoParams.data.editedTasks.push(todoParamsTask);
+};
+
+const addTaskToDestroyedTasks = (todoParamsTask) => {
+  if (!todoParams.data.destroyedTasks) {
+    todoParams.data.destroyedTasks = [];
+  }
+  todoParams.data.destroyedTasks.push(todoParamsTask);
+};
+
+
 const updateTaskInTodoParams = (task) => {
-  const tempTodoParams = todoParams;
-  const tasks = tempTodoParams.data.tasks;
+  const tasks = todoParams.data.tasks;
   const indexOfTask = task.data.indexInTasks;
   const todoParamsTask = tasks.at(indexOfTask);
   Object.assign(todoParamsTask.data, task.data);
+
+  if (todoParamsTask.data.id) {
+    addTaskToEditedTasks(todoParamsTask);
+  }
 };
 
 const destroyTaskInTodoParams = (task) => {
-  const tempTodoParams = todoParams;
-  const tasks = tempTodoParams.data.tasks;
+  const tasks = todoParams.data.tasks;
   const indexOfTask = task.data.indexInTasks;
+  const todoParamsTask = tasks.at(indexOfTask);
   tasks.splice(indexOfTask, 1);
+  
+  if (todoParamsTask.data.id) {
+    addTaskToDestroyedTasks(todoParamsTask);
+  }
 };
 
 const Controller = createController('tasks', Task, params);
