22f754465ed023f3364a8006c1b8c09037c04fe6
Dylan Palmboom <nofxboy1234@gmail.com>
2024-03-22 20:38:32 +0200

    Remove commented code

--- src/modelError.mjs
+++ src/modelError.mjs
@@ -9,8 +9,3 @@ function createModelError(description) {
 }
 
 export { createModelError };
-
-// const error = createModelError('Name cannot be blank');
-// console.log(error.description);
-// error.description = 'z';
-// console.log(error.description);
