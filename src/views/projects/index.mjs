92d92fa37f1b031be0f145d5187a78c15f7b9424
Dylan Palmboom <nofxboy1234@gmail.com>
2024-03-28 15:34:30 +0200

    Remove mock projects index view

--- src/views/projects/index.mjs
+++
@@ -1,9 +0,0 @@
-// Publishers and subscribers don't need to know each other.
-// They simply communicate with the help of message queues.
-const index = {
-  update(data) {
-    console.log(`Add ${data.name} to projects index`);
-  },
-};
-
-export { index };
