004aa7654dae978a6d4760549aa55d01d8824195
Dylan Palmboom <nofxboy1234@gmail.com>
2024-04-02 14:17:10 +0200

    Delete pubsub and observer pattern tests

--- old/observer/index.mjs
+++
@@ -1,9 +0,0 @@
-import { subject } from './subject.mjs';
-import { observer as observer1 } from './observer1.mjs';
-import { observer as observer2 } from './observer2.mjs';
-import { addObserver } from './subject.mjs';
-
-addObserver(observer1);
-addObserver(observer2);
-
-subject.click();
