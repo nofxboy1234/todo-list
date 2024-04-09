320a47e21871b892311d9a42d535fa2926ea9f3a
Dylan Palmboom <nofxboy1234@gmail.com>
2024-04-09 12:45:22 +0200

    Remove test model instances

--- src/index.js
+++ src/index.js
@@ -12,49 +12,5 @@ function createDefaultProject() {
   }
 }
 
-function createTestProjects() {
-  const project1 = new Project('Project 1');
-  if (project1.save()) {
-    console.log(`Saved ${project1.name} successfully`);
-  } else {
-    project1.errors.forEach((error) => console.log(error.description));
-  }
-
-  const todo1 = new Todo('Todo 1', 'This is Todo 1.', '2024-03-28', 'low', 1);
-  if (todo1.save()) {
-    console.log(`Saved ${todo1.title} successfully`);
-  } else {
-    todo1.errors.forEach((error) => console.log(error.description));
-  }
-
-  const todo2 = new Todo(
-    'Todo 2',
-    'This is Todo 2.',
-    '2024-03-29',
-    'medium',
-    1
-  );
-  if (todo2.save()) {
-    console.log(`Saved ${todo2.title} successfully`);
-  } else {
-    todo2.errors.forEach((error) => console.log(error.description));
-  }
-
-  const todo3 = new Todo('Todo 3', 'This is Todo 3.', '2024-03-29', 'high', 1);
-  if (todo3.save()) {
-    console.log(`Saved ${todo3.title} successfully`);
-  } else {
-    todo3.errors.forEach((error) => console.log(error.description));
-  }
-
-  const project2 = new Project('Project 2');
-  if (project2.save()) {
-    console.log(`Saved ${project2.name} successfully`);
-  } else {
-    project2.errors.forEach((error) => console.log(error.description));
-  }
-}
-
 createDefaultProject();
-createTestProjects();
 createLayout();
