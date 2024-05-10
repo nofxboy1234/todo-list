868d160bbf09e757c6e87253a5b029284f5349ff
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-10 15:43:22 +0200

    Install "@babel/plugin-transform-arrow-functions" and run babel from cli

--- src/testBabel.js
+++ src/testBabel.js
@@ -1,1 +1,3 @@
 [1, 2, 3].map((n) => n + 1);
+
+const hello = () => console.log("hello world!");
---
+++ src/testBabelCompiled.js
@@ -0,0 +1,6 @@
+[1, 2, 3].map(function (n) {
+  return n + 1;
+});
+const hello = function () {
+  return console.log("hello world!");
+};
